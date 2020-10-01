96e2b59fb0d51b6af68d51b024b50ed8dc2b48de
vanyalarin <vanyalarin2000@gmail.com>
2020-10-02 01:41:45 +0300

    Added CSS moduls

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React, {Component} from 'react'
 import Layout from './hoc/Layout/Layout'
+import Quiz from './containers/Quiz/Quiz'
 
 class App extends Component {
   render() {
     return (
       <Layout>
-
-
-        <div style={{width: 400, border: '1px solid black'}}>
-          <h1>Layout works</h1>
-        </div>
+        <Quiz />
       </Layout>
     )
   }
